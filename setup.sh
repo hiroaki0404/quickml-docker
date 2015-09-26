62baaefab42433091b0e7e90072e0d7267f84240
Hiroaki Abe <hiroaki0404@gmail.com>
2015-09-26 16:32:37 +0900

    Make working directory

--- setup.sh
+++ setup.sh
@@ -8,3 +8,6 @@ if [ ! -z "$CONFIRM_ML_CREATION" ]; then
   sed -i "s/\(^.*:confirm_ml_creation => \)false\(.*\)$/\1true\2/" /etc/quickml/quickmlrc
 fi
 
+mkdir /var/lib/quickml
+chown list:list /var/lib/quickml
+
